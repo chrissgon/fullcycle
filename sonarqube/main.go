9c07581ead4a7ffd5b87f46a98e12f901bb79a6d
Christopher Gonçalves <christopher.goncalves2002@hotmail.com.br>
2022-11-13 18:44:08 -0300

    feat: add sonarqube module

---
+++ sonarqube/main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(sum(1, 1))
+}
+
+func sum(a int, b int) int {
+	return a + b
+}
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func mult(a int, b int) int {
+	return a * b
+}
+
+func div(a int, b int) int {
+	return a / b
+}
+
+func resto(a int, b int) int {
+	return a % b
+}
+
+func restoa(a int, b int) int {
+	return a % b
+}
+
+func restob(a int, b int) int {
+	return a % b
+}
+
+func restoc(a int, b int) int {
+	return a % b
+}
+
+func restod(a int, b int) int {
+	return a % b
+}
+
+func restoe(a int, b int) int {
+	return a % b
+}
+
+func restof(a int, b int) int {
+	return a % b
+}
